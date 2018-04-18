3163998052303471989240e2d5efc7bb9576030e
ryutah <ryuta0221@gmail.com>
2018-04-18 11:15:51 +0900

    Add: スクレイピング処理スケルトン

---
+++ app/scraping.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""Web scraping module
+
+Todo:
+    * Implement ScrapingResult class.
+    * Implement Scraping class.
+"""
+
+
+class ScrapingResult(object):
+    """Scraping result store.
+
+    Attributes:
+        result (str): scraping result(site's html).
+        links (list[str]): links in scraped site.
+    """
+    # TODO(implement scraping result)
+    pass
+
+
+def scraping(url):
+    """Scraping given url site.
+
+    Args:
+        url (str): scraping site url.
+
+    Returns:
+        ScrapingResult: result of scraping.
+    """
+    # TODO(implement scraping proc.)
+    return ScrapingResult()
